4984012adb9ecde7d330a6a31851de9ef413c719
Abdiel Licona <abdiel.licona@021digital.marketing>
2023-11-16 16:27:28 -0600

    Primeras plantillas

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@include('layouts._partials.header_admin')
+
+@section('content')
+    <div class="container">
+        <h1>
+            {{-- Hola {{ auth()->user()->name }} --}}
+            Hola Nombre del usuario
+        </h1>
+    </div>
+
+    <div class="container">
+        <div class="d-flex justify-content-between">
+            <h3>Lista de usuarios</h3>
+            <a href="" class="btn btn-primary">Crear usuario</a>
+        </div>
+        @include('users._partials.table')
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+
+Route::get('/users', function () {
+    return view('users.index');
+})->name('users');
